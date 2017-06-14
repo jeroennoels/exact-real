bf402bc9601b68e0ddf82d4c70caa9fd42c063ce
jeroennoels <jeroen.noels@telenet.be>
2017-06-14 21:04:27 +0200

    intermediate commit

--- test/Ternary/Recursive.hs
+++ test/Ternary/Recursive.hs
@@ -3,9 +3,11 @@
 
 module Ternary.Recursive where
 
+import Data.Functor.Compose
+import Control.Applicative
 import Ternary.Core.Digit (T2(..))
-import Ternary.Sampling.Expression (Binding)
-import Ternary.Sampling.Calculation hiding (Depth, refine)
+import Ternary.Sampling.Expression (Var(Var), Binding)
+import Ternary.Sampling.Calculation hiding (Depth, refine, variables)
 
 data Analysis = IncDepth | Bailout | Inconclusive
               deriving Show
@@ -13,15 +15,13 @@ data Analysis = IncDepth | Bailout | Inconclusive
 class Refinable r s | r -> s, s -> r  where
   refine :: r -> Either r s
   proceed :: Binding T2 -> s -> Either r s
+  variables :: s -> [Var]
 
 class Analyze r where
   analyze :: Depth -> r -> Analysis
 
 
 newtype Walk = Walk [T2] deriving Show
-
-cons :: T2 -> Walk -> Walk
-cons a (Walk as) = Walk (a:as)
 
 type Walk2 = (Walk, Walk)
 type Acc = [Walk2]
@@ -35,7 +35,8 @@ instance Refinable Ready Stalled where
   refine BeginLevel = Right Stall
   refine NextLevel = Left BeginLevel
   proceed _ Stall = Left NextLevel
-
+  variables _ = [Var 0, Var 1]
+  
 instance Analyze Ready where
   analyze depth _ | depth > limit = Bailout  
   analyze _ BeginLevel = Inconclusive
@@ -45,7 +46,7 @@ instance Analyze Ready where
 type Depth = Int
 
 limit :: Depth
-limit = 2
+limit = 8
 
 recurse :: (Refinable r s, Analyze r) =>
            Walk2 -> Depth -> Either r s -> Acc -> Acc
@@ -56,17 +57,36 @@ recurse c depth (Left refinable) acc =
    IncDepth -> recurse c (depth+1) (refine refinable) acc 
    Inconclusive -> recurse c depth (refine refinable) acc
 -- right
+
 recurse c depth (Right stalled) acc =
   let go :: Acc -> (Walk2, Binding T2) -> Acc
       go accum (extended, binding) =
         recurse extended depth (proceed binding stalled) accum
-  in foldl go acc (branching c)
+  in foldl go acc (branching c (variables stalled))
 
 
-prepare :: Walk2 -> T2 -> (Walk2, Binding T2)
-prepare (a,b) d = ((cons d a, b), const d)
+cons :: Maybe T2 -> Walk -> Walk
+cons Nothing w = w
+cons (Just a) (Walk as) = Walk (a:as)
 
-branching :: Walk2 -> [(Walk2, Binding T2)]
-branching c = map (prepare c) [M1, O0, P1]
 
--- recurse (Walk [], Walk []) 1 (Left BeginLevel) []
+branching :: Walk2 -> [Var] -> [(Walk2, Binding T2)]
+branching c@(a,b) vars = liftA2 (prepare c)
+                   (branch vars (Var 0) a)
+                   (branch vars (Var 1) b)
+
+branch :: [Var] -> Var -> Walk -> [Maybe T2]
+branch vars var _ | not (var `elem` vars) = [Nothing]
+branch _ _ a | a `longerThan` 1 = [Just O0]
+branch _ _ _ = map Just [M1,O0,P1]
+
+longerThan (Walk xs) n = not $ null (drop n xs) 
+
+prepare :: Walk2 -> Maybe T2 -> Maybe T2 -> (Walk2, Binding T2)
+prepare (a,b) u v  = ((cons u a, cons v b), bind u v)
+
+bind (Just u) _ (Var 0) = u
+bind _ (Just v) (Var 1) = v
+bind _ _ _ = error "Ternary.Recursive (bind)"
+
+test = recurse (Walk [], Walk []) 1 (Left BeginLevel) []
